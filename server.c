d55d7b446e51b8e4ce99a9812fe6019f1b36b1e8
Lxela <acourcoul.ir2019@esaip.org>
2018-04-18 14:18:41 +0200

    Update server.c

--- server.c
+++ server.c
@@ -22,11 +22,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
-#include <sys/socket.h>
-#include <string.h>
-#include <sys/socket.h>
+
+#ifdef __WIN32__
+# include <winsock2.h>
+#else
+# include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#endif
+
+#include <string.h>
 #include <float.h>
 
 
