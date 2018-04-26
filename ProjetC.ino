b814f25ff5f2bfd7860098ce3dd82432294a0580
vincent8132 <vincent.degouy@live.fr>
2018-04-26 11:08:21 +0200

    Add files via upload

--- ProjetC.ino
+++ ProjetC.ino
@@ -41,7 +41,7 @@ void setup() {
 
 
 void loop() {
-    char acquisition[10] = "stop"; // bien ici ??????
+    char acquisition[10] = "stop";
     
     char* mess = malloc(10*sizeof(char));
     char* acqu_state = malloc(10*sizeof(char));
@@ -57,85 +57,106 @@ void loop() {
     int n;
     int ret;
     float avg;
+    char test;
 
 
               
-  buffer = Serial.readString();
-  char order = buffer[1];
-  if(buffer != NULL){          
-  Serial.print("I received something: ");
-  Serial.println("message : ");
-  Serial.println(buffer);
-  
+    buffer = Serial.readString();
+    char order = buffer[1];
+    if(buffer != NULL){          
+        Serial.print("I received something: ");
+        Serial.println("message : ");
+        Serial.println(buffer);
 
     switch(order) {
         case 'S':
+            
             strcpy(mess, "AS0Z");
             memcpy(buf, &buffer[2], 2);
-            buf[2] = '\0';
-            entry = buf-'0';
+            buf[2] = '\0';            
+            entry = atoi(buf);
+            Serial.print("entry = ");
+            Serial.println(entry);
             memcpy(buf, &buffer[4], 1);
             buf[1] = '\0';
-            n = buf-'0';
+            n = atoi(buf);
+            Serial.print("n = ");
+            Serial.println(n);
             ret = setEntry(&entry, &n);
-            break;
+              break;
+              
         case 'E':
+            Serial.println("rentré dans case E");
             strcpy(mess, "AE0");
             memcpy(buf, &buffer[2], 2);
             buf[2] = '\0';
-            entry = buf-'0';
+            entry = atoi(buf);
             ret = readEntry(&entry);
-            strcat(mess, ret);
+            Serial.print("ret : ");
+            Serial.println(ret);
+            test = char(&ret);
+            strcat(mess, &test);
             strcat(mess, "Z");
+            Serial.print("mess : ");
+            Serial.println(mess);
             break;
+            
         case 'C':
+            Serial.println("rentré dans case c");
             strcpy(mess,"AC0");
             sprintf(buf, "%.2f", avg);
+            if (avg < 10) 
+            strcat(buf, "0");
             strcat(mess, buf);
             strcat(mess, "Z");
+            Serial.println("sort du case c");
             break;
+            
         case 'M':
+            Serial.println("rentré dans case m");
             //to get the differents datas from the message
             memcpy(subbuf, &buffer[2], 1);      // c'est le v
-            subbuf[2] = '\0';
+            subbuf[1] = '\0';
            
             memcpy(subbuf2, &buffer[3], 2);     // c'est le temps
             subbuf2[2] = '\0';
            
-            memcpy(subbuf3, &buffer[5],2);      // c'est le num
+            memcpy(subbuf3, &buffer[5], 1);      // c'est le num
             subbuf3[1] = '\0';
-           
-           
-            printf("acquisition = %s\n", acquisition);
- 
+            
+            //Serial.print("acquisition = ");
+            //Serial.print(acquisition);
+            
             //to start the acquisition of the temperature
             if (strcmp(subbuf,"1") == 0) {
-                printf("\nAcquisition started witch %s seconds of interval.\n",subbuf2);
-                printf("We calculate median with %s data\n\n. ",subbuf3);
-                int n = subbuf3-'0';
-                getAverage(tabtemperature, &n);  // tabtemperature est le tableau qui a les temperature
-           
+                
+                
+                Serial.print("Acquisition started with an interval in seconds of : ");
+                Serial.println(subbuf2);
+                Serial.print("We calculate median with a number of data of : ");
+                Serial.println(subbuf3);
+                int n = atoi(subbuf3);
+                avg = getAverage(tabtemperature, &n);  // tabtemperature est le tableau qui a les temperature
+                strcpy(acquisition,"start");
+                
             }
             //stop the acquisition of the temperature
             else {
                 strcpy(mess,"AM0Z");
                 strcpy(acquisition,"stop");
-                printf("mess = %s\n", mess);
+                Serial.print("mess = ");
+                Serial.println(mess);
             }
-            printf("acquisition = %s\n", acquisition);
+            Serial.print("acquisition is now = ");
+            Serial.println(acquisition);
             break;
         default:
-            printf("invalid order\n");
+            Serial.println("invalid order \n");
             strcpy(mess,"erreuuuur");
  
     }
 
 
-
-
-
-
-  
   }
 
 
@@ -158,18 +179,25 @@ void loop() {
   delay(500); // a changer selon demande
 }
 
-int setEntry(int *val, int *entry) {
-    int ret;
- 
+
+
+int setEntry(int *entry, int *val) {
+    int ret = 0;
+    bool state;
+    if (*val == 0){
+      state = LOW;} 
+      
+      else if(*val==1){
+        state = HIGH;}
+
     pinMode(entry,OUTPUT);
-    digitalWrite(entry,val);
- 
+    digitalWrite(*entry,state);
+    
     return ret;
 }
  
 int readEntry (int *entry) {
     int ret;
- 
     pinMode(entry,INPUT);
     return digitalRead(entry);
 }
